a8c41ab1e1f74088aee79452ee349bfed3a61d4e
jackyboyjo <github noreply email@useres.noreply.github.com>
2024-03-19 20:43:28 -0600

    ICE 4

--- src/hdl/stoplight_fsm.vhd
+++ src/hdl/stoplight_fsm.vhd
@@ -72,23 +72,33 @@ end stoplight_fsm;
 architecture stoplight_fsm_arch of stoplight_fsm is 
 	
 	-- create register signals with default state yellow (10)
+	signal f_Q, f_Q_next : std_logic_vector(1 downto 0) := "10";
   
 begin
 	-- CONCURRENT STATEMENTS ----------------------------
 	-- Next state logic
-	
+	f_Q_next(1) <= ((not i_C and f_Q(0)) and not f_Q(1));
+	f_Q_next(0) <= (i_C and not f_Q(1));
 	
 	-- Output logic
-	
+	o_G <= (f_Q(0) and not f_Q(1));
+	o_Y <= (not f_Q(0) and f_Q(1));
+	o_R <= (not f_Q(1) and not f_Q(0)) or
+	       (f_Q(0) and f_Q(1));
 	-------------------------------------------------------	
 	
 	-- PROCESSES ----------------------------------------	
 	-- state memory w/ asynchronous reset ---------------
-	register_proc : process (  )
+	register_proc : process (i_clk, i_reset)
 	begin
 			--Reset state is yellow
-
-
+    if i_reset = '1' then
+        f_Q <= "10"; 
+        --f_Q_next <= "10";
+    elsif (rising_edge(i_clk)) then
+        f_Q <= f_Q_next;
+        end if;
+       
 	end process register_proc;
 	-------------------------------------------------------
 	
